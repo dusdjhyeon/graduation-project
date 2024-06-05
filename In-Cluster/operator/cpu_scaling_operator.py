9b274a156438b2c4e3fa2683e5df8000fd01f04b
dusdjhyeon <dusdj0813@gmail.com>
2024-06-05 13:39:25 +0900

    feat: add adjust resources periodically

--- In-Cluster/operator/cpu_scaling_operator.py
+++ In-Cluster/operator/cpu_scaling_operator.py
@@ -24,6 +24,25 @@ def configure(settings: kopf.OperatorSettings, **_):
     config.load_incluster_config()
     settings.posting.level = 'INFO'
 
+@kopf.timer('pods', interval=60)
+def adjust_resources_periodically(namespace, name, logger, **kwargs):
+    query = 'workload:adas_long_term_cpu_usage:q95_max_rate_1m'
+    try:
+        results = query_prometheus(query)
+        for result in results:
+            pod_namespace = result['metric'].get('namespace')
+            pod_name = result['metric'].get('pod')
+            cpu_usage = result['value'][1]
+
+            new_resources = client.V1ResourceRequirements(
+                requests={"cpu": f"{float(cpu_usage) * 1000}m"},
+                limits={"cpu": f"{float(cpu_usage) * 2000}m"}
+            )
+
+            if pod_namespace == namespace and pod_name == name:
+                adjust_pod_resources(pod_name, pod_namespace, new_resources)
+    except Exception as e:
+        logger.error(f"Exception when querying Prometheus: {e}")
 
 if __name__ == '__main__':
     kopf.run()
