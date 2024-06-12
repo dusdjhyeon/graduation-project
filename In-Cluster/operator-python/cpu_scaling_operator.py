5e14355dae4b0a006df12762f32f972ae5e4e5a8
dusdjhyeon <dusdj0813@gmail.com>
2024-06-12 17:58:12 +0900

    feat: add debugging level

--- In-Cluster/operator-python/cpu_scaling_operator.py
+++ In-Cluster/operator-python/cpu_scaling_operator.py
@@ -15,15 +15,24 @@ def adjust_task_resources(workflow_name, namespace, task_name, new_resources):
     version = 'v1alpha1'
     plural = 'workflows'
 
-    # Woking if pod_name==workflow_task_name
     try:
         workflow = api_instance.get_namespaced_custom_object(group, version, namespace, plural, workflow_name)
+        updated = False
         for template in workflow['spec']['templates']:
             if template['name'] == task_name:
-                template['container']['resources'] = new_resources
-        
-        api_instance.replace_namespaced_custom_object(group, version, namespace, plural, workflow_name, workflow)
-        print(f"Workflow {workflow_name} in {namespace} has been adjusted.")
+                if 'container' in template:
+                    template['container']['resources'] = new_resources
+                elif 'script' in template:
+                    template['script']['resources'] = new_resources
+                updated = True
+                break
+
+        if updated:
+            api_instance.replace_namespaced_custom_object(group, version, namespace, plural, workflow_name, workflow)
+            print(f"Workflow {workflow_name} in {namespace} has been adjusted.")
+        else:
+            print(f"Task {task_name} not found in workflow {workflow_name}.")
+
     except client.exceptions.ApiException as e:
         print(f"Exception when calling CustomObjectsApi->replace_namespaced_custom_object: {e}")
 
@@ -40,16 +49,16 @@ def adjust_resources_periodically(namespace, name, logger, **kwargs):
         for result in results:
             workflow_namespace = result['metric'].get('namespace')
             workflow_name = result['metric'].get('workflow')
-            task_name = result['metric'].get('pod')  
+            task_name = result['metric'].get('pod')
             cpu_usage = result['value'][1]
 
-            new_resources = {
-                "requests": {"cpu": f"{float(cpu_usage) * 1000}m"},
-                "limits": {"cpu": f"{float(cpu_usage) * 2000}m"}
-            }
-
             if workflow_namespace == namespace and workflow_name == name:
+                new_resources = {
+                    "requests": {"cpu": f"{float(cpu_usage) * 1000}m"},
+                    "limits": {"cpu": f"{float(cpu_usage) * 2000}m"}
+                }
                 adjust_task_resources(workflow_name, workflow_namespace, task_name, new_resources)
+
     except Exception as e:
         logger.error(f"Exception when querying Prometheus: {e}")
 
